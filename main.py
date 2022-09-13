ac7e731b27470843653398ccf41052825627ebdf
tcd76 <c0088537@newcastle.ac.uk>
2022-09-13 19:09:27 +0100

    Get the player details added

--- main.py
+++ main.py
@@ -17,3 +17,24 @@ def get_web_req(url_req):
     soup = BeautifulSoup(page, 'html.parser')
 
     return soup
+
+
+"""
+Get the player information
+"""
+
+soup = get_web_req("shooting")
+dataframe = pd.DataFrame(columns=details_header)
+for table in soup.find_all(id="stats_shooting"):
+    for row in (soup.find_all("tr")):
+        player = []
+        count = 0
+        for item in row.find_all("td"):
+            if count < len(details_header):
+                player.append(item.text)
+            count += 1
+
+
+        if len(player) == len(details_header):
+            dataframe = dataframe.append(pd.DataFrame([player], columns=details_header), ignore_index=True)
+
